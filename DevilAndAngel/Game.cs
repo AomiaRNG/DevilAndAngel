066675cd5d1b1ed0bcebda9e600841a7b63fba9a
Br0Ken <vladik8882@mail.ru>
2022-01-07 11:16:30 +1000

    Add initial files

---
+++ DevilAndAngel/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevilAndAngel
+{
+    enum Direction
+    {
+        Right,
+        RightUp,
+        RightDown,
+        Left,
+        LeftUp,
+        LeftDown,
+        Up,
+        UpRight,
+        UpLeft,
+        Down,
+        DownRight,
+        DownLeft
+    }
+    class Game
+    {
+        
+    }
+}
